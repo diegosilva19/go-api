e5427ecb7cf21dc6cef7d003a97462b755906b3e
Diego <diego.silva_19@yahoo.com.br>
2024-01-18 02:56:47 -0300

    init create logger and gorm structure

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-	"fmt"
-
+	"github.com/diegosilva19/go-api/config"
 	"github.com/diegosilva19/go-api/router"
 )
 
+var (
+	logger config.Logger
+)
+
 func main() {
-	fmt.Print("ola mundo")
+	logger = *config.GetLogger("main")
+
+	logger.Infof("ola mundo")
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("error incial %v", err)
+		//panic(err)
+		return
+	}
 	router.Initialize()
 }
